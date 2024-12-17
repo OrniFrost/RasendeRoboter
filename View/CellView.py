a6c1e56969fbb6dee86bd18179244194a0506b53
gabin B. <gab.blond@hotmail.com>
2024-12-17 13:43:25 +0100

    Add star shape for cells

--- View/CellView.py
+++ View/CellView.py
@@ -6,6 +6,7 @@ class CellView:
     polygon_pawn = [9, 46, 13, 41, 11, 36, 8, 33, 8, 29, 11, 26, 15, 23, 13, 20, 11, 18, 10, 16, 10, 12, 12, 9, 15, 7,
                     19, 5, 30, 5, 34, 7, 37, 9, 39, 12, 39, 16, 38, 18, 36, 20, 34, 23, 38, 26, 41, 29, 41, 33, 38, 36,
                     36, 41, 38, 46]
+    polygon_star = [5,45,15,28,5,19,16,14,24,3,25,3,33,14,44,19,34,28,44,45,25,37,24,37]
 
     def __init__(self, parent, cell):
         self.cell = cell
@@ -40,4 +41,4 @@ class CellView:
                 case "triangle":
                     self.canvas.create_polygon([25, 10, 10, 40, 40, 40], fill=color, outline='')
                 case "star":
-                    self.canvas.create_oval(10, 15, 40, 30, fill=color, outline='')
+                    self.canvas.create_polygon(self.polygon_star, fill=color, outline='')
